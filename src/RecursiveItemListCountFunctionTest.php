7acf0cc52e1968212fdfc74ae01f6a3fd4859479
Erwan Haquet <ehaquet.dev@gmail.com>
2021-02-19 17:11:15 +0100

    add a method to recursively count a list of items

---
+++ src/RecursiveItemListCountFunctionTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Exercise 4.2 : Write a recursive item list count function.
+ *
+ * @param array $items an array of items.
+ * @param int   $count the current element count.
+ * @return int
+ */
+function countElements(array $items, int $count = 0): int
+{
+    // Base case
+    if (empty($items)) {
+        return $count;
+    }
+
+    // Recursive case
+    array_pop($items);
+    $count++;
+
+    return countElements($items, $count);
+}
+
+final class RecursiveItemListCountFunctionTest extends TestCase
+{
+    public function testEmptyListReturnsZero(): void
+    {
+        $this->assertEquals(0, countElements([]));
+    }
+
+    public function testOneItemListReturnsOne(): void
+    {
+        $this->assertEquals(1, countElements([1]));
+        $this->assertEquals(1, countElements([-2]));
+        $this->assertEquals(1, countElements([403746]));
+    }
+
+    public function testMultipleItemsListReturnsItemListCount(): void
+    {
+        $this->assertEquals(2, countElements([1, 2]));
+        $this->assertEquals(8, countElements([1, 2, 3, 4, 5, 6, 7, 8]));
+    }
+}
--- src/RecursiveSumFunctionTest.php
+++ src/RecursiveSumFunctionTest.php
@@ -5,21 +5,22 @@ use PHPUnit\Framework\TestCase;
 /**
  * Exercise 4.1 : Write a recursive sum function.
  *
- * @param array $array an array of numbers
- * @return int|mixed
+ * @param array $array an array of numbers.
+ * @return int
  */
 function sum(array $array): int
 {
+    // Base case
     if (empty($array)) {
         return 0;
     }
 
+    // Recursive case
     if (count($array) === 1) {
         return $array[0];
     }
 
     $value = array_pop($array);
-
     return $value + sum($array);
 }
 
