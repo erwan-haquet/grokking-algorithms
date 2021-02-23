d4a8c443cc8186d325cb71e40eec5248e525cd7a
Erwan Haquet <ehaquet.dev@gmail.com>
2021-02-23 17:26:43 +0100

    improve binary search test, and it fails :'(

--- src/RecursiveBinarySearchTest.php
+++ src/RecursiveBinarySearchTest.php
@@ -5,41 +5,52 @@ use PHPUnit\Framework\TestCase;
 /**
  * Exercise 4.4 : Write a binary search using recursive function.
  *
- * @param int      $needle   The value to find.
- * @param array    $haystack An array of sorted values.
- * @param int|null $guess    Last guessed index.
- * @return int
+ * @param int      $search The value to find.
+ * @param array    $values An array of sorted values.
+ * @param int|null $guess  Last guessed index.
+ * @param int      $i      The number of iterations to find the value.
+ * @return array
  */
-function binarySearch(int $needle, array $haystack, int $guess = null): int
+function binarySearch(int $search, array $values, int $guess = null, $i = 0): array
 {
+    $i++;
+
+    // Initialize guess if it is the first loop.
     if (null === $guess) {
-        $guess = intval(count($haystack) / 2);
+        $guess = intval(count($values) / 2);
     }
 
     // Recursive case
-    if ($needle < $haystack[$guess]) {
+    if ($search < $values[$guess]) {
         $newGuess = intval($guess * 0.5);
-        return binarySearch($needle, $haystack, $newGuess);
-    } else if ($needle > $haystack[$guess]) {
+        return binarySearch($search, $values, $newGuess, $i);
+    } else if ($search > $values[$guess]) {
         $newGuess = intval($guess * 1.5);
-        return binarySearch($needle, $haystack, $newGuess);
+        return binarySearch($search, $values, $newGuess, $i);
     }
 
     // Base case
-    return $guess;
+    return [
+        'result' => $guess,
+        'iterations' => $i
+    ];
 }
 
 final class RecursiveBinarySearchTest extends TestCase
 {
     public function testItReturnsCorrectValue(): void
     {
-        $haystack = range(1, 100);
-        $this->assertEquals(9, binarySearch(10, $haystack));
+        $values = range(1, 100);
+        $expected = ['result' => 9, 'iterations' => 5];
+        $this->assertEquals($expected, binarySearch(10, $values));
 
-        $haystack = range(1, 1024);
-        $this->assertEquals(21, binarySearch(22, $haystack));
+        //TODO: fix assertion, since binary search should result in O(log n)
+        $values = range(1, 1024);
+        $expected = ['result' => 21, 'iterations' => 1];
+        $this->assertEquals($expected, binarySearch(22, $values));
 
-        $haystack = range(4, 4);
-        $this->assertEquals(0, binarySearch(4, $haystack));
+        $values = range(4, 4);
+        $expected = ['result' => 0, 'iterations' => 1];
+        $this->assertEquals($expected, binarySearch(4, $values));
     }
 }
